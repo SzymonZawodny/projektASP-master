64bc29aa20705f648035cd36a559cd2798b1f8ee
Szymon <zawodny.szymon@gmail.com>
2017-01-27 01:04:51 +0100

    projekt

---
+++ Teatr/Teatr/Helpery/SwojeHelpery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Teatr.Helpery
+{
+    public static class SwojeHelpery
+    {
+      
+
+        public static string WstawTekst(string tekst)
+        {
+            return String.Format("Witamy użytkownika " + tekst + " na stronie projektu!");
+        }
+    }
+}
